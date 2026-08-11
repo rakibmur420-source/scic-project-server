3fb3a1ac2739d0ccf17cf4ae6da9ee09d180af09
rakib <rakibmur420@gmail.com>
2026-08-11 20:18:44 +0600

    fix

--- src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { UserController } from "../services/user/user.controller";
-
-const router = express.Router();
-
-router.post("/register", UserController.register);
-router.post("/login", UserController.login);
-
-export const AuthRoutes = router;
